91d3d0a3233fbf26d9a2b1ae9ef9c226f46de564
jmartschinke <79451982+jmartschinke@users.noreply.github.com>
2021-03-12 13:29:38 +0100

    Added problematic file

---
+++ PropertyChangedBug/Problem.cs
@@ -0,0 +1,21 @@
+﻿using PropertyChanged;
+
+namespace PropertyChangedBug
+{
+    [AddINotifyPropertyChangedInterface]
+    public class BaseClass
+    {
+        [OnChangedMethod(nameof(ProcessPropertyWasChanged))]
+        public object Property { get; set; }
+
+        public void ProcessPropertyWasChanged()
+        {
+            // Do something
+        }
+    }
+
+    public class DerivedClass : BaseClass
+    {
+
+    }
+}
